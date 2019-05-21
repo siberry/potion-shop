a78a37f2356063de9e89ab884856f8bf8e10db5f
siberry <siberry226@gmail.com>
2019-05-21 16:50:55 -0400

    solution

--- src/PotionTile.js
+++ src/PotionTile.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
-/* 
+/*
 Write an abstracted component that can be used in place of InventoryTile AND ShopTile
 
-Consider what the tiles have in common and what they do not: 
-- For things that are the same, copy and pasting works just fine! 
-- For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
+Consider what the tiles have in common and what they do not:
+- For things that are the same, copy and pasting works just fine!
+- For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents
 */
 
 const PotionTile = (props) => {
+  const handleClick = () => {
+    if (props.function === "inventory") {
+      props.sell(props.potion.id)
+    } else {
+      props.addToInventory(props.potion.id)
+    }
+  }
   return (
-    null
+    <div key={props.potion.id} className={props.function === "inventory" ? "inventory-card" : "card"}>
+      <div onClick={handleClick} className="image-wrapper">
+        <img className="image" alt={props.potion.name} src={props.potion.image_url} />
+      </div>
+      {props.children}
+    </div>
   )
 }
 
